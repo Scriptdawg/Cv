764d8657ec66fa2bafa6c5d78278795907221411
Paul Oosterhoff <pauloosterhoff@gmail.com>
2023-03-28 10:54:22 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  // res.writeHead(200, {"Content-Type": "text/plain"});
+  // res.end("NodeJS - render/github");
+
+  // HTML Response - File Stream
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.createReadStream(__dirname + "/index.html").pipe(res);
+});
+
+const PORT = process.env.Port || 3000;
+server.listen(PORT, () => console.log("Server is running on port 3000"));
