72ba3fd5ec7da7846ad91324342f10fc5521fb36
Paul Oosterhoff <pauloosterhoff@gmail.com>
2023-03-28 10:53:41 -0600

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("NodeJS - render/github");
-});
-
-const PORT = process.env.Port || 3000;
-server.listen(PORT, () => console.log("Server is running on port 3000"));
